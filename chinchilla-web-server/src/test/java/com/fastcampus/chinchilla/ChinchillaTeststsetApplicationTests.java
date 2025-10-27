8c163f7a5669a6dcc760141d47b08e576e0db0a7
김동균 <eastVirusDa@gmail.com>
2025-10-27 12:13:24 +0900

    MySQL 도커 연결 완료

---
+++ chinchilla-web-server/src/test/java/com/fastcampus/chinchilla/ChinchillaTeststsetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.chinchilla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinchillaTeststsetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ spring-web/main/java/com.chinchilla/ChinchillaApllication.java
@@ -0,0 +1,4 @@
+package com.chinchilla;
+
+public class ChinchillaApllication {
+}
