5117e92935f7b77908ba46bd3717847b63499b87
onuyyy <qazxsw9026@naver.com>
2025-11-03 12:11:37 +0900

    feat : 마크다운 응답형식으로 변경

--- chinchilla-web-server/src/main/java/com/fastcampus/chinchilla/dto/AgentResponse.java
+++ chinchilla-web-server/src/main/java/com/fastcampus/chinchilla/dto/AgentResponse.java
@@ -1,36 +1,20 @@
 package com.fastcampus.chinchilla.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.List;
 import java.util.Map;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AgentResponse {
     private String answer;
+    private String answerHtml;
     private List<Map<String, Object>> sources;
     private Map<String, Object> metadata;
-
-    public AgentResponse() {}
-
-    public String getAnswer() {
-        return answer;
-    }
-
-    public void setAnswer(String answer) {
-        this.answer = answer;
-    }
-
-    public List<Map<String, Object>> getSources() {
-        return sources;
-    }
-
-    public void setSources(List<Map<String, Object>> sources) {
-        this.sources = sources;
-    }
-
-    public Map<String, Object> getMetadata() {
-        return metadata;
-    }
-
-    public void setMetadata(Map<String, Object> metadata) {
-        this.metadata = metadata;
-    }
 }
